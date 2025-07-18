c9d1c9d1d3d4a54d631a70cdf882f35ec9f3f3cf
Cameron Barnes <cameron_barnes@outlook.com>
2025-07-18 16:33:37 -0700

    Basic project setup and TextNode class

---
+++ src/textnode.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+class TextType(Enum):
+	PLAIN = "plain",
+	BOLD = "bold",
+	ITALIC = "italic",
+	CODE = "code",
+	LINK = "link",
+	IMAGE = "image"
+
+class TextNode:
+	def __init__(self, text: str, text_type: TextType, url=None) -> None:
+		self.text = text
+		self.text_type = text_type
+		self.url = url
+	
+	def __eq__(self, value: object, /) -> bool:
+		return self is value and self.text == value.text and self.text_type is value.text_type and self.url == value.url
+	
+	def __repr__(self) -> str:
+		return f"TextNode({self.text}, {self.text_type}, {self.url})"
