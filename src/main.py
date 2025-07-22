73a834891929a3609c4dce0cb8337c735ed731f6
Cameron Barnes <cameron_barnes@outlook.com>
2025-07-22 14:28:25 -0700

    Fixed recursive page generation, added more example content

--- src/main.py
+++ src/main.py
@@ -4,20 +4,20 @@ from markdown_converter import extract_title, markdown_to_html_node
 import os
 def main():
 	setup_public_dir()
-	generate_pages("./content", "./template.html", "./public")
+	generate_pages_recursive("./content", "./template.html", "./public")
 
-def generate_pages(from_dir: str, template_path: str, dest_dir: str):
+def generate_pages_recursive(from_dir: str, template_path: str, dest_dir: str):
 	if not os.path.exists(from_dir) or not os.path.isdir(from_dir):
-		raise ValueError("invalid from_dir")
-	if not os.path.exists(dest_dir) or not os.path.isdir(dest_dir):
-		raise ValueError("invalid dest_dir")
+		raise ValueError(f"invalid from_dir: {from_dir}")
+	if os.path.exists(dest_dir) and not os.path.isdir(dest_dir):
+		raise ValueError(f"invalid dest_dir: {dest_dir}")
 	if not os.path.exists(template_path) or not os.path.isfile(template_path):
-		raise ValueError("invalid template_path")
+		raise ValueError(f"invalid template_path: {template_path}")
 	for entry in os.listdir(from_dir):
 		from_path = os.path.join(from_dir, entry)
 		dest_path = os.path.join(dest_dir, entry)
 		if os.path.isdir(from_path):
-			generate_pages(from_path, template_path, dest_path)
+			generate_pages_recursive(from_path, template_path, dest_path)
 		else:
 			generate_page(from_path, template_path, dest_path)
 
